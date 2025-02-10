ed5ed275e085485b80eccf860dc7ba0bd3a30361
RPNRPNS <133812979+RPNRPNS@users.noreply.github.com>
2025-02-10 12:43:21 +0100

    Update AguaPequeñaPsyco.js

--- AguaPequeñaPsyco.js
+++ AguaPequeñaPsyco.js
@@ -540,7 +540,7 @@ document.write("" + textosemana[diasemana] + " " + diames + " de " + textomes[me
 
 })(jQuery);
     $("#stream1").icast({
-	    URL: "https://technoplayerserver.net:8102",
+	    URL: "https://radio.technoplayer2radioserver.org:8050",
 		version: "2",
        })
        
